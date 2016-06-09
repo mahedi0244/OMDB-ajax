a6e9d47642b2bec966b49e6f9b56f1dfa2821a92
Mahedi <mahedi0244@gmail.com>
2016-06-09 13:31:07 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+$(document).ready(function(){
+  $("#search").on("submit",function(e){
+    e.preventDefault();
+    var formData = {
+      't'              : $('input[name=t]').val(),
+    };
+    $.ajax({   
+      url: "http://www.omdbapi.com/?",   
+      data: formData,
+      dataType: "json",   
+      success: function(response) {     
+        console.log(response);  
+        $("#movie-title").html("Title : "+response.Title);   
+        $("#poster").attr("src", response.Poster);
+        $("#genre").html("Genrer : "+response.Genre);  
+        $("#year").html("Year : "+response.Year);
+        $("#actors").html("Actors : "+response.Actors);
+      } 
+    });
+  });
+});
+
+function myFunction() {
+
+    var formData = {
+      't'              : $('input[name=t]').val(),
+    };
+    //alert($( "usrinput" ).length);
+    //if (document.getElementById("usrinput").length == 3){
+      
+      $.ajax({   
+        url: "http://www.omdbapi.com/?",   
+        data: formData,
+        dataType: "json",   
+        success: function(response) {     
+          console.log(response);  
+          $("#movie-title").html("Title : "+response.Title);   
+          $("#poster").attr("src", response.Poster);
+          $("#genre").html("Genrer : "+response.Genre);  
+          $("#year").html("Year : "+response.Year);
+          $("#actors").html("Actors : "+response.Actors);
+        } 
+      });
+    //}
+}
